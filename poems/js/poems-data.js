92107ba0dc0bf475cfb1e7e817a264917ccf3989
Banhpro <thanhkietnguyen5@gmail.com>
2026-03-04 13:42:05 +0700

    poem

--- poems/js/poems-data.js
+++ poems/js/poems-data.js
@@ -1,26 +1,30 @@
 const basePath = "list_poem/";
 const poems = [
-{
-    title: "Mẹ Già",
-    name: "me_gia",
-    date: "12/05/2023",
-    author: "Bành",
-    category: ["GIA ĐÌNH","QUÊ HƯƠNG"],
-    type: "LỤC BÁT",
-    audio: true
-},
-{
-    title: "Quê Hương",
-    name: "ve_que",
-    date: "01/08/2023",
-    author: "Bành",
-    category: "QUÊ HƯƠNG",
-    type: "TỰ DO",
-    audio: false
-}
+    {
+        title: "Mẹ Già",
+        name: "me_gia",
+        date: "12/05/2023",
+        author: "Bành",
+        category: ["GIA ĐÌNH", "QUÊ HƯƠNG"],
+        type: "LỤC BÁT",
+        audio: true,
+        content: "",  // Nội dung sẽ được lấy từ HTML
+    },
+    {
+        title: "Quê Hương",
+        name: "ve_que",
+        date: "01/08/2023",
+        author: "Bành",
+        category: "QUÊ HƯƠNG",
+        type: "TỰ DO",
+        audio: false,
+        content: "",  // Nội dung sẽ được lấy từ HTML
+    }
 ];
+
+// Đảm bảo đường dẫn chính xác
 poems.forEach(p => {
-    p.file  = `${basePath}${p.name}/index.html`;
-    p.image = `${basePath}${p.name}/image.jpg`;
-    p.audioFile = `${basePath}${p.name}/audio.mp3`;
+    p.file = `${basePath}${p.name}/index.html`; // Đảm bảo đường dẫn file HTML
+    p.image = `${basePath}${p.name}/image.jpg`; // Đảm bảo đường dẫn file hình ảnh
+    p.audioFile = `${basePath}${p.name}/audio.mp3`; // Đảm bảo đường dẫn file âm thanh
 });
